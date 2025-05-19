6d3f053cdc57643fed7cbcface0ed104a661d823
Davide Faconti <davide.faconti@gmail.com>
2025-05-19 12:13:14 +0200

    WIP

--- cmake/FindLZ4.cmake
+++ cmake/FindLZ4.cmake
@@ -12,30 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Finds liblz4.
 #
-# - Try to find Facebook zstd library
-# This will define
-# ZSTD_FOUND
-# ZSTD_INCLUDE_DIR
-# ZSTD_LIBRARY
+# This module defines:
+# LZ4_FOUND
+# LZ4_INCLUDE_DIR
+# LZ4_LIBRARY
 #
 
-find_path(ZSTD_INCLUDE_DIR NAMES zstd.h)
+find_path(LZ4_INCLUDE_DIR NAMES lz4.h)
 
-find_library(ZSTD_LIBRARY_DEBUG NAMES zstdd zstd_staticd)
-find_library(ZSTD_LIBRARY_RELEASE NAMES zstd zstd_static)
+find_library(LZ4_LIBRARY_DEBUG NAMES lz4d)
+find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
 
 include(SelectLibraryConfigurations)
-SELECT_LIBRARY_CONFIGURATIONS(ZSTD)
+SELECT_LIBRARY_CONFIGURATIONS(LZ4)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
-    ZSTD DEFAULT_MSG
-    ZSTD_LIBRARY ZSTD_INCLUDE_DIR
+    LZ4 DEFAULT_MSG
+    LZ4_LIBRARY LZ4_INCLUDE_DIR
 )
 
-if (ZSTD_FOUND)
-    message(STATUS "Found Zstd: ${ZSTD_LIBRARY}")
+if (LZ4_FOUND)
+    message(STATUS "Found LZ4: ${LZ4_LIBRARY}")
 endif()
 
-mark_as_advanced(ZSTD_INCLUDE_DIR ZSTD_LIBRARY)
+mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
