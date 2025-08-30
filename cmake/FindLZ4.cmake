613d6dc6d3fa49322950d71c6802c8af773e4592
Davide Faconti <davide.faconti@gmail.com>
2025-08-30 17:03:10 +0200

    try

--- cmake/FindLZ4.cmake
+++ cmake/FindLZ4.cmake
@@ -14,10 +14,10 @@
 
 # Finds liblz4.
 #
-# This module defines: LZ4_FOUND LZ4_INCLUDE_DIR LZ4_LIBRARY
+# This module defines: LZ4_FOUND LZ4_INCLUDE_DIRS LZ4_LIBRARY
 #
 
-find_path(LZ4_INCLUDE_DIR NAMES lz4.h)
+find_path(LZ4_INCLUDE_DIRS NAMES lz4.h)
 
 find_library(LZ4_LIBRARY_DEBUG NAMES lz4d)
 find_library(LZ4_LIBRARY_RELEASE NAMES lz4)
@@ -26,10 +26,10 @@ include(SelectLibraryConfigurations)
 select_library_configurations(LZ4)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LZ4 DEFAULT_MSG LZ4_LIBRARY LZ4_INCLUDE_DIR)
+find_package_handle_standard_args(LZ4 DEFAULT_MSG LZ4_LIBRARY LZ4_INCLUDE_DIRS)
 
 if(LZ4_FOUND)
   message(STATUS "Found LZ4: ${LZ4_LIBRARY}")
 endif()
 
-mark_as_advanced(LZ4_INCLUDE_DIR LZ4_LIBRARY)
+mark_as_advanced(LZ4_INCLUDE_DIRS LZ4_LIBRARY)
