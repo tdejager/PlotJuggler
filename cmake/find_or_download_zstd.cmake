d6d3b3879612260fa904285578dd7ac6e2e4f98a
Davide Faconti <davide.faconti@gmail.com>
2025-09-29 00:57:22 +0200

    fix zstd installing headers and libraries

--- cmake/find_or_download_zstd.cmake
+++ cmake/find_or_download_zstd.cmake
@@ -10,25 +10,31 @@ function(find_or_download_zstd)
       URL https://github.com/facebook/zstd/archive/refs/tags/v1.5.7.zip
       DOWNLOAD_ONLY YES)
 
-    set(ZSTD_BUILD_STATIC ON CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_SHARED OFF CACHE BOOL " " FORCE)
-    set(ZSTD_LEGACY_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_PROGRAMS OFF CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_TESTS OFF CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_CONTRIB OFF CACHE BOOL " " FORCE)
-    set(ZSTD_BUILD_EXAMPLES OFF CACHE BOOL " " FORCE)
-    set(ZSTD_MULTITHREAD_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_LEGACY_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_ZLIB_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_LZ4_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_LZMA_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_ZDICT_SUPPORT OFF CACHE BOOL " " FORCE)
-    set(ZSTD_PROGRAMS "" CACHE STRING " " FORCE)
+    set(LIBRARY_DIR ${zstd_SOURCE_DIR}/lib)
+    file(GLOB CommonSources ${LIBRARY_DIR}/common/*.c)
+    file(GLOB CommonHeaders ${LIBRARY_DIR}/common/*.h)
 
-    add_subdirectory(${zstd_SOURCE_DIR}/build/cmake ${zstd_BINARY_DIR})
+    file(GLOB CompressSources ${LIBRARY_DIR}/compress/*.c)
+    file(GLOB CompressHeaders ${LIBRARY_DIR}/compress/*.h)
+
+    file(GLOB DecompressSources ${LIBRARY_DIR}/decompress/*.c)
+    file(GLOB DecompressHeaders ${LIBRARY_DIR}/decompress/*.h)
+
+    set(Sources ${CommonSources} ${CompressSources} ${DecompressSources})
+    set(Headers ${PublicHeaders} ${CommonHeaders} ${CompressHeaders} ${DecompressHeaders})
+
+    add_compile_options(-DZSTD_DISABLE_ASM)
+
     set(ZSTD_FOUND TRUE)
 
+    add_library(libzstd_static STATIC ${Sources} ${Headers})
+    set_property(TARGET libzstd_static PROPERTY POSITION_INDEPENDENT_CODE ON)
+
     add_library(zstd::libzstd_static INTERFACE IMPORTED)
+    set_target_properties(zstd::libzstd_static
+      PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${zstd_SOURCE_DIR}/lib
+                 INTERFACE_LINK_LIBRARIES libzstd_static)
+
     set_target_properties(zstd::libzstd_static PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${zstd_SOURCE_DIR}/lib
         INTERFACE_LINK_LIBRARIES libzstd_static)
